ad68583f9b12a01b27c711416a3e52f46f05df2b
Wouter Buckens <wouter@epicteddy.com>
2021-08-23 17:45:41 +0200

    Formatting

--- endless-galaxy-server/app/Validators/MarketSellOrderCreateValidator.ts
+++ endless-galaxy-server/app/Validators/MarketSellOrderCreateValidator.ts
@@ -1,15 +1,15 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { schema } from '@ioc:Adonis/Core/Validator';
 
 export default class MarketSellOrderCreateValidator {
-  constructor (protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-	  planetId: schema.number(),
-	  itemType: schema.string(),
-	  price: schema.number(),
-	  amount: schema.number(),
-  });
+	public schema = schema.create({
+		planetId: schema.number(),
+		itemType: schema.string(),
+		price: schema.number(),
+		amount: schema.number(),
+	});
 
-  public messages = {}
+	public messages = {};
 }
