1f0a121685bc03073494a40774aa20abcae82183
Wouter Buckens <wouter@epicteddy.com>
2021-08-25 15:41:35 +0200

    Increase size of ship button

--- endless-galaxy-client/components/ShipButton.vue
+++ endless-galaxy-client/components/ShipButton.vue
@@ -2,7 +2,7 @@
 	<nuxt-link
 		:to="localePath({ name: 'game-ship-shipId', params: { shipId: ship.id } })"
 		class="p-1.5 rounded border border-gray-600 hover:bg-gray-700 focus:bg-gray-700 focus-visible:ring-2 ring-cyan-500">
-		<component :is="icon" class="h-4" />
+		<component :is="icon" class="h-5" />
 		<div v-if="ship.movement_minutes_remaining !== null" class="mt-1 h-1 bg-gray-600 rounded-full overflow-hidden">
 			<div class="h-1 bg-violet-500" :style="`width:${ movementProgress * 100 }%`" />
 		</div>
