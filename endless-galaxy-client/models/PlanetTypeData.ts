a1b90955b2c04bb65986f4a7d8fcca3ddaf48d5d
Wouter Buckens <wouter@epicteddy.com>
2021-08-27 13:47:23 +0200

    Update planet data interface

--- endless-galaxy-client/models/PlanetTypeData.ts
+++ endless-galaxy-client/models/PlanetTypeData.ts
@@ -6,7 +6,8 @@ export type PlanetTypeId = string;
 export default interface PlanetTypeData {
 	id: PlanetTypeId;
 
-	factories: FactoryTypeId[];
 	buildCostModifier: number;
 	recipeOutputModifiers: Record<ItemTypeId, number>;
+	factories: FactoryTypeId[];
+	scavenge: ItemTypeId[];
 }
