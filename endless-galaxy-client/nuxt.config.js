f2312c6b687b200fde5a5704874b9ae9ec1e598c
Wouter Buckens <wouter@epicteddy.com>
2021-08-27 18:19:17 +0200

    Add meta description in client

--- endless-galaxy-client/nuxt.config.js
+++ endless-galaxy-client/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: packageJson.description || '' },
+			{ hid: 'description', name: 'description', content: 'Interplanetary Traders & Entrepreneurs' },
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
