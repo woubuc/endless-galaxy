caddec4e41775572ca2fddbddf448fcf1b6ae644
Wouter Buckens <wouter@epicteddy.com>
2021-08-28 13:57:45 +0200

    Remove Nuxt favicon

--- endless-galaxy-client/nuxt.config.js
+++ endless-galaxy-client/nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 			{ hid: 'description', name: 'description', content: 'Interplanetary Traders & Entrepreneurs' },
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			// { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
 			{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 			{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
 			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&family=JetBrains+Mono:wght@400;600&display=swap' },
